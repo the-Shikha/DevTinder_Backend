9a59bf0e8dacb767a1912f68ae95fedf22b28c24
shikha <shikhakumari152019@gmail.com>
2025-05-12 13:13:09 +0530

    Refactor user route handlers to use middleware for improved structure and logging by using multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,39 +1,34 @@
 const express=require("express");
 const app=express();
 
-//This is giving error
-// app.get("/ab+c",(req,res)=>{
-//     res.send("user +")
-// })
+app.get("/user",
+    [(req,res,next)=>{
+        console.log("1st route handler")
+        //res.send("1st response")
+        next();
+    },
+    (req,res,next)=>{
+        console.log("2nd route handler")
+        //res.send("2nd response")
+        next();
+    },
+    (req,res,next)=>{
+        console.log("3rd route handler")
+        //res.send("3rd response")
+        next();
+    },
+    (req,res,next)=>{
+        console.log("4th route handler")
+        //res.send("4th response")
+        next();
+    },
+    (req,res,next)=>{
+        console.log("5th route handler")
+        res.send("5th response")
+        next();
+    }]
 
-// app.get(/a/,(req,res)=>{
-//     res.send("Regex")
-// })
-
-// app.get(/.*fly$/,(req,res)=>{
-//     res.send("Regex end with fly")
-// })
-
-// app.get("/user",(req,res)=>{
-//     console.log(req.query)
-//     return res.send({firstName:"Shikha",lastName:"Das"})
-// })
-
-app.get("/user/:userId/:name/:work",(req,res)=>{
-    console.log(req.params)
-    return res.send({firstName:"Shikha",lastName:"Das"})
-})
-
-app.post("/user",(req,res)=>{
-    return res.send("Data is pushed successfully")
-})
-app.delete("/user",(req,res)=>{
-    return res.send("Data is deleted successfully")
-})
-
-// app.use("/",(req,res)=>{
-//    return res.send("Home page of DevTinder")
-// })
+)
 
 app.listen(5000,()=>{
     console.log("Server is listening on port no 5000")
