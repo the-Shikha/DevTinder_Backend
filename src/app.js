ac8d716d7f20172bd62673c9c422ecb4affaed21
shikha <shikhakumari152019@gmail.com>
2025-05-12 12:29:58 +0530

    GET, POST,PATCH,DELETE API used

--- src/app.js
+++ src/app.js
@@ -1,17 +1,39 @@
 const express=require("express");
 const app=express();
 
-app.use("/hello",(req,res)=>{
-    return res.send("Hello page of DevTinder")
+//This is giving error
+// app.get("/ab+c",(req,res)=>{
+//     res.send("user +")
+// })
+
+// app.get(/a/,(req,res)=>{
+//     res.send("Regex")
+// })
+
+// app.get(/.*fly$/,(req,res)=>{
+//     res.send("Regex end with fly")
+// })
+
+// app.get("/user",(req,res)=>{
+//     console.log(req.query)
+//     return res.send({firstName:"Shikha",lastName:"Das"})
+// })
+
+app.get("/user/:userId/:name/:work",(req,res)=>{
+    console.log(req.params)
+    return res.send({firstName:"Shikha",lastName:"Das"})
 })
 
-app.use("/test",(req,res)=>{
-    return res.send("Test page of DevTinder")
+app.post("/user",(req,res)=>{
+    return res.send("Data is pushed successfully")
+})
+app.delete("/user",(req,res)=>{
+    return res.send("Data is deleted successfully")
 })
 
-app.use("/",(req,res)=>{
-   return res.send("Home page of DevTinder")
-})
+// app.use("/",(req,res)=>{
+//    return res.send("Home page of DevTinder")
+// })
 
 app.listen(5000,()=>{
     console.log("Server is listening on port no 5000")
