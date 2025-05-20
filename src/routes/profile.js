ede43338a5adc516467a9c62b8c5b6a05f0ef703
shikha <shikhakumari152019@gmail.com>
2025-05-21 00:48:06 +0530

    Add password change functionality with validation and error handling

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,6 +3,7 @@ const profileRouter=express.Router();
 const {userAuth}=require("../middlewares/Auth");
 const User = require("../models/user");
 const {validateEditProfileData}=require("../utils/validation")
+const bcrypt=require("bcrypt")
 
 profileRouter.get("/profile/view",userAuth,async(req,res)=>{
     try{
@@ -39,6 +40,34 @@ profileRouter.patch("/profile/edit",userAuth,async(req,res)=>{
     }
     catch(err){
         res.status(400).send("Issue while editing the profile : "+err.message)
+    }
+})
+
+profileRouter.patch("/profile/changePassword",userAuth,async(req,res)=>{
+    try{
+        const user=req.user;
+        const {prevPassword,newPassword}=req.body;
+        const {password}=user
+
+        if (!prevPassword || !newPassword) {
+            return res.status(400).send("Both previous and new password are required.");
+        }
+
+        // Check if new password is different from previous
+        if (prevPassword === newPassword) {
+            return res.status(400).send("New password must be different from the old password.");
+        }
+        const isPrevPasswordValid=await bcrypt.compare(prevPassword,password)
+        if(!isPrevPasswordValid){
+            res.status(400).send("Old password is not correct")
+        }
+        const hashNewPwd=await bcrypt.hash(newPassword,10);
+        const updateUserPwdDb=await User.findByIdAndUpdate({_id:user._id},{password:hashNewPwd},{new:true})
+        res.send(`Password changed successfully
+                data : ${updateUserPwdDb}` )
+    }
+    catch(err){
+        res.status(400).send("Issue while changing password: "+err.message)
     }
 })
 
